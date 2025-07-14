7ab554d8ead70bfceb88b28185ab67c39e63990a
DevEmmottDarMar <marceloemmott@icloud.com>
2025-07-14 10:42:15 -0400

    Fix WebSocket JWT authentication - Configure proper JWT secret in EventsModule

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -4,9 +4,21 @@ import { EventsGateway } from './events.gateway';
 import { EventsService } from './events.service';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from '../users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [JwtModule.register({}), UsersModule],
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRATION_TIME') },
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    ConfigModule,
+  ],
   providers: [EventsGateway, EventsService],
   exports: [EventsGateway, EventsService],
 })
